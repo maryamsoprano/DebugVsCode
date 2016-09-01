8203ae3a9d121bbd5b3b8313f261cde86dce69dc
maryamsoprano <maryam.gharibi@soprano.com.au>
2016-09-02 09:49:54 +1000

    Create my-app.ts

---
+++ app/lib/components/my-app.ts
@@ -0,0 +1,40 @@
+import { Component, ViewEncapsulation, OnInit } from '@angular/core';
+
+ 
+
+
+ 
+
+
+
+@Component({
+
+    selector: 'sei-app',
+
+    templateUrl: './app/lib/components/ my-app.html',
+
+    directives: [],
+
+    providers: [
+
+    ]
+
+})
+
+
+
+export class SeiApp   {
+
+
+
+    constructor( ) {
+
+      
+
+        this.className = 'my-app';
+
+         
+
+    }
+
+}
